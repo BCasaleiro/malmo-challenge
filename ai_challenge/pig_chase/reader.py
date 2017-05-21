de97d7a43f63699be5e764243e1dcca2be8e2462
j-godinho <joaogodinho_4@hotmail.com>
2017-05-21 22:30:21 +0100

    Some tests and Q matrix

--- ai_challenge/pig_chase/reader.py
+++ ai_challenge/pig_chase/reader.py
@@ -14,20 +14,9 @@ for line in lines:
 		val.append(line_split[len(line_split)-1])
 
 h1 = [0 for i in range(51)]
-h2 = [0 for i in range(51)]
-h3 = [0 for i in range(51)]
-h4 = [0 for i in range(51)]
 
 for i, v in enumerate(val):
-	if(i<((len(val)/4))):
-		h1[int(float(v))+25] = h1[int(float(v))+25] + 1 
-	elif (i<((len(val)/4)*2)):
-		h2[int(float(v))+25] = h2[int(float(v))+25] + 1
-	elif (i<((len(val)/4)*3)):
-		h3[int(float(v))+25] = h3[int(float(v))+25] + 1
-	elif (i<((len(val)/4)*4)):
-		h4[int(float(v))+25] = h4[int(float(v))+25] + 1
-
+	h1[int(float(v))+25] = h1[int(float(v))+25] + 1 
 
 def print_array(array):
 	for a in array:
@@ -35,6 +24,3 @@ def print_array(array):
 	print "-----"
 
 print_array(h1)
-print_array(h2)
-print_array(h3)
-print_array(h4)
